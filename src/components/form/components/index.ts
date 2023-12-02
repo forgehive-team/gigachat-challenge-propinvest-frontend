0176231e9cf84959f83d20fd2cb07f3bfb8fd38c
JollyBanny <rootki7@protonmail.com>
2023-12-02 22:03:58 +1000

    chore(form): added index

---
+++ src/components/form/components/index.ts
@@ -0,0 +1,7 @@
+import FormButton from './FormButton.vue'
+import FormComponent from './FormComponent.vue'
+import FormInput from './FormInput.vue'
+import FormTab from './FormTab.vue'
+import TheForm from './TheForm.vue'
+
+export { FormButton, FormComponent, FormInput, FormTab, TheForm }
