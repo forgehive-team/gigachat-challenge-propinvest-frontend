2f29e8ebe2af91faf672ebe4ad429e5a5f31ef7e
JollyBanny <rootki7@protonmail.com>
2023-12-02 17:21:43 +1000

    wip: removed useless files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import type { Component } from 'vue'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -12,10 +14,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: (): Component => import('../views/AboutView.vue')
     }
   ]
 })
