0d63554f2cd74061a32ccfb161ff79a8c6415df0
JollyBanny <rootki7@protonmail.com>
2023-12-03 02:36:59 +1000

    chore(FormMartialStatus): completed

--- src/components/form/FormMaritalStatus.vue
+++ src/components/form/FormMaritalStatus.vue
@@ -1,43 +1,40 @@
 <template>
   <TheForm :data="{ title: 'Семейное положение' }">
-    <FormSelector v-model="martialStatus" :items="martialStatuses" />
+    <FormSelector v-model="selectedStatus" :items="martialStatusList" />
+    <FormSelector v-model="hasChildern" :items="hasChildernList" />
+    <FormInput
+      v-if="hasChildern.value === 'yes'"
+      v-model="childernNumber"
+      placeholder="Количество детей"
+      type="number"
+    />
     <FormButton title="Далее" :action="next" class="bg-accent text-primary" />
   </TheForm>
 </template>
 
 <script setup lang="ts">
-import { FormButton, FormSelector, TheForm } from '@/components/form/components'
+import { FormButton, FormInput, FormSelector, TheForm } from '@/components/form/components'
 
 import type { FormSelectorItem } from '@/types'
 import { ref } from 'vue'
 
 const emit = defineEmits<{ (e: 'submit'): void }>()
 
-const martialStatuses: FormSelectorItem[] = [
-  {
-    key: '',
-    name: 'Выберите семейное положение',
-    disabled: true,
-    value: ''
-  },
-  {
-    key: 'wtf-1',
-    name: 'WTF-1',
-    value: 'wtf-1-value'
-  },
-  {
-    key: 'wtf-2',
-    name: 'WTF-2',
-    value: 'wtf-2-value'
-  },
-  {
-    key: 'wtf-3',
-    name: 'WTF-3',
-    value: 'wtf-3-value'
-  }
+const martialStatusList: FormSelectorItem[] = [
+  { key: '', name: 'Ваше семейное положение', disabled: true, value: '' },
+  { key: 'married', name: 'Женат / замужем', value: 'married' },
+  { key: 'single', name: 'Холост', value: 'single' }
 ]
 
-const martialStatus = ref<FormSelectorItem>(martialStatuses[0])
+const hasChildernList: FormSelectorItem[] = [
+  { key: '', name: 'У вас есть дети?', disabled: true, value: '' },
+  { key: 'yes', name: 'Да', value: 'yes' },
+  { key: 'no', name: 'Нет', value: 'no' }
+]
+
+const selectedStatus = ref<FormSelectorItem>(martialStatusList[0])
+const hasChildern = ref<FormSelectorItem>(hasChildernList[0])
+const childernNumber = ref<number>(0)
 
 const next = (): void => {
   emit('submit')
