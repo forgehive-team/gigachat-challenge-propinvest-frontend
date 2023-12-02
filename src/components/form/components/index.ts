e8a89ceda794e2fc6dd1b04c72cf4e3641ffea81
JollyBanny <rootki7@protonmail.com>
2023-12-03 00:52:08 +1000

    chore(form): added FormSelector to index

--- src/components/form/components/index.ts
+++ src/components/form/components/index.ts
@@ -1,7 +1,8 @@
 import FormButton from './FormButton.vue'
 import FormComponent from './FormComponent.vue'
 import FormInput from './FormInput.vue'
+import FormSelector from './selector/FormSelector.vue'
 import FormTab from './FormTab.vue'
 import TheForm from './TheForm.vue'
 
-export { FormButton, FormComponent, FormInput, FormTab, TheForm }
+export { FormButton, FormComponent, FormInput, FormSelector, FormTab, TheForm }
