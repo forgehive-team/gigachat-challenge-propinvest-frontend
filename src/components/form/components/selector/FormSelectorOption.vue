25196999d8e065f681fbbc23dab996966c536e0b
JollyBanny <rootki7@protonmail.com>
2023-12-03 00:46:22 +1000

    feat(form): added FormSelector

---
+++ src/components/form/components/selector/FormSelectorOption.vue
@@ -0,0 +1,14 @@
+<template>
+  <li
+    :class="{ 'cursor-pointer': !item.disabled, 'text-disabled-500': item.disabled }"
+    class="w-full py-5 border-t border-hover-500 first-of-type:border-secondary"
+  >
+    <span> {{ item.name }} </span>
+  </li>
+</template>
+
+<script setup lang="ts">
+import type { FormSelectorItem } from '@/types'
+
+defineProps<{ item: FormSelectorItem }>()
+</script>
