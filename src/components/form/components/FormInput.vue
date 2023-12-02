81b8a5206da602343bb36c7b66b823a577518bab
JollyBanny <rootki7@protonmail.com>
2023-12-02 22:03:36 +1000

    feat(form): added FormInput

---
+++ src/components/form/components/FormInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <FormComponent class="border border-secondary">
+    <input
+      v-model="value"
+      :disabled="disabled"
+      :placeholder="placeholder"
+      class="w-full px-7 py-4 outline-none font-light"
+    />
+  </FormComponent>
+</template>
+
+<script setup lang="ts">
+import { FormComponent } from '.'
+import { computed } from 'vue'
+
+const props = withDefaults(defineProps<{ modelValue: string; disabled?: boolean; placeholder?: string }>(), {
+  disabled: false
+})
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
+</script>
