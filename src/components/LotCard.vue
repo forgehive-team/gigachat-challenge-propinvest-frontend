bd7c8f50198fe44b7ca6d72e4441f9e288b44f03
tzhovtyi <tzzzhovtyy@gmail.com>
2023-12-02 21:58:11 +0300

    Add swiper

---
+++ src/components/LotCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-card p-[30px] rounded">
+    <h3 class="text-2xl leading-tight text-secondary mt-[30px] mb-5">Квартира в одноквартирном доме</h3>
+    <div class="flex w-full">
+      <span class="mt-2 text-secondary text-lg font-light flex-shrink-0">Площадь (м2)</span>
+      <span class="w-full border-b border-solid border-[#AFBCD0]"></span>
+      <span class="mt-2 text-secondary text-lg font-light flex-shrink-0">00013</span>
+    </div>
+    <div class="flex w-full">
+      <span class="mt-2 text-secondary text-lg font-light flex-shrink-0">Этаж</span>
+      <span class="w-full border-b border-solid border-[#AFBCD0]"></span>
+      <span class="mt-2 text-secondary text-lg font-light flex-shrink-0">34</span>
+    </div>
+    <div class="flex w-full">
+      <span class="mt-2 text-secondary text-lg font-light flex-shrink-0">Комнаты</span>
+      <span class="w-full border-b border-solid border-[#AFBCD0]"></span>
+      <span class="mt-2 text-secondary text-lg font-light flex-shrink-0">5</span>
+    </div>
+    <div class="mt-[30px] flex gap-[30px] justify-between items-end">
+      <span class="text-secondary text-[28px] flex-shrink-0">16 000 000 руб.</span>
+      <button
+        class="text-[18px] font-medium leading-5 tracking-wide text-center py-[15px] px-[50px] bg-accent text-white rounded max-w-[290px]"
+      >
+        УЗНАТЬ ПОДРОБНЕЕ
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
+import LotCard from './LotCard.vue'
 import ProfileInfoCard from './ProfileInfoCard.vue'
 import ProjectCard from './ProjectCard.vue'
 import TheFooter from './TheFooter.vue'
 import TheHeader from './TheHeader.vue'
+import YandexMap from './YandexMap.vue'
 
-export { ProfileInfoCard, ProjectCard, TheFooter, TheHeader }
+export { LotCard, ProfileInfoCard, ProjectCard, TheFooter, TheHeader, YandexMap }
