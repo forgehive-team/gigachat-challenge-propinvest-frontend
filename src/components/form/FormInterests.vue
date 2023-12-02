f1fe7b66445d35dd231157b7e31e3419b36f1161
JollyBanny <rootki7@protonmail.com>
2023-12-03 02:01:55 +1000

    feat(form): added Interests form

---
+++ src/components/form/FormInterests.vue
@@ -0,0 +1,25 @@
+<template>
+  <TheForm :data="{ title: 'Регистрация' }">
+    <FormTextarea v-model="interests" placeholder="Введите ваши предпочтения" />
+    <FormCheckbox
+      v-model="agree"
+      text="Я принимаю правила Я принимаю правила Я принимаю правила Я принимаю правила Я принимаю правила"
+    />
+    <FormButton title="Готово" :action="registate" class="bg-accent text-primary" />
+  </TheForm>
+</template>
+
+<script setup lang="ts">
+import { FormButton, FormCheckbox, FormTextarea, TheForm } from '@/components/form/components'
+
+import { ref } from 'vue'
+
+const emit = defineEmits<{ (e: 'submit'): void }>()
+
+const interests = ref('')
+const agree = ref(false)
+
+const registate = (): void => {
+  emit('submit')
+}
+</script>
--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,6 +1,7 @@
+import FormInterests from './FormInterests.vue'
 import FormMaritalStatus from './FormMaritalStatus.vue'
 import FormProfile from './FormProfile.vue'
 import FormReg from './FormReg.vue'
 import FormRole from './FormRole.vue'
 
-export { FormMaritalStatus, FormProfile, FormReg, FormRole }
+export { FormInterests, FormMaritalStatus, FormProfile, FormReg, FormRole }
