442450dc2e6ae4318d9bed78b414f63c052e1acf
JollyBanny <rootki7@protonmail.com>
2023-12-03 02:02:08 +1000

    chore(FormView): imported Interests form

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,16 +1,17 @@
 <template>
   <main class="container min-h-[calc(100dvh-270px)] mx-auto flex justify-center">
     <div class="mt-14 mb-24">
-      <FormReg v-if="formStep === 1" @submit="++formStep" />
+      <!-- <FormReg v-if="formStep === 1" @submit="++formStep" />
       <FormRole v-else-if="formStep === 2" @submit="++formStep" />
       <FormProfile v-else-if="formStep === 3" @submit="++formStep" />
-      <FormMaritalStatus v-else-if="formStep === 4" @submit="++formStep" />
+      <FormMaritalStatus v-else-if="formStep === 4" @submit="++formStep" /> -->
+      <FormInterests @submit="++formStep" />
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import { FormMaritalStatus, FormProfile, FormReg, FormRole } from '@/components/form'
+import { FormInterests, FormMaritalStatus, FormProfile, FormReg, FormRole } from '@/components/form'
 
 import { ref } from 'vue'
 
