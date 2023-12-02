246e50c64132367d09bab8da577c0c7fa234ed6d
JollyBanny <rootki7@protonmail.com>
2023-12-02 22:03:48 +1000

    feat(form): added FormButton

---
+++ src/components/form/components/FormButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <FormComponent class="border transition-all">
+    <button class="w-full h-16 text-lg font-medium uppercase" @click="onClick">
+      {{ title }}
+    </button>
+  </FormComponent>
+</template>
+
+<script setup lang="ts">
+import { FormComponent } from '.'
+
+const props = defineProps<{ title: string; action: Function; value?: number | string | object }>()
+
+const onClick = (): void => {
+  if (props.value) {
+    props.action(props.value)
+  } else {
+    props.action()
+  }
+}
+</script>
